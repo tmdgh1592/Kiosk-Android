1914639da248a959c09ef55a68d1cea4bc5c4f90
gkrlsanjf <gkrlsanjf@naver.com>
2022-06-22 20:31:38 +0900

    디렉토리 변경

--- kiosk/app/src/main/java/com/swuniv/agefree/presentation/detection/FdActivity.java
+++ kiosk/app/src/main/java/com/swuniv/agefree/presentation/detection/FdActivity.java
@@ -1,4 +1,4 @@
-package com.swuniv.agefree.presentation.detection.ui.start;
+package com.swuniv.agefree.presentation.detection;
 
 import android.content.Context;
 import android.content.Intent;
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.swuniv.agefree.R;
+import com.swuniv.agefree.presentation.detection.DetectionBasedTracker;
 import com.swuniv.agefree.presentation.detection.MainActivity;
 
 import org.opencv.android.BaseLoaderCallback;
