ad0b6fa4ef882a7b6dafaf527a1759b4828b7480
tmdgh1592 <tmdgh1592@naver.com>
2022-06-23 12:05:45 +0900

    원(ex. 1,000) 표시 Extension 추가

--- kiosk/app/src/main/java/com/swuniv/agefree/presentation/detection/utils/Extensions.kt
+++ kiosk/app/src/main/java/com/swuniv/agefree/presentation/detection/utils/Extensions.kt
@@ -1,13 +1,9 @@
 package com.swuniv.agefree.presentation.detection.utils
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.os.Environment
 import android.util.Log
 import android.widget.Toast
-import java.io.ByteArrayOutputStream
-import java.io.File
-import java.io.FileOutputStream
+import java.text.DecimalFormat
 
 fun String.printLog(tag: String? = null) {
     Log.d("+++ $tag", this)
@@ -21,26 +17,7 @@ fun Context.showToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
 
-fun Bitmap.toFile(bitmap: Bitmap, fileNameToSave: String): File? { // File name like "image.png"
-    //create a file to write bitmap data
-    var file: File? = null
-    return try {
-        file = File(Environment.getExternalStorageDirectory().toString() + File.separator + fileNameToSave)
-        file.createNewFile()
-
-        //Convert bitmap to byte array
-        val bos = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 0, bos) // YOU can also save it in JPEG
-        val bitmapdata = bos.toByteArray()
-
-        //write the bytes in file
-        val fos = FileOutputStream(file)
-        fos.write(bitmapdata)
-        fos.flush()
-        fos.close()
-        file
-    } catch (e: Exception) {
-        e.printStackTrace()
-        file // it will return null
-    }
+fun Int.toWon(): String {
+    val format = DecimalFormat("#,###")
+    return "${format.format(this)}원"
 }
