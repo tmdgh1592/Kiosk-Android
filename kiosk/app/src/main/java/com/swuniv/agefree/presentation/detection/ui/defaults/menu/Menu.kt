37472a049d7ab14719efaf9ef72231f6cb25c873
tmdgh1592 <tmdgh1592@naver.com>
2022-06-23 11:44:29 +0900

    Menu 데이터 클래스 필드 수정

--- kiosk/app/src/main/java/com/swuniv/agefree/presentation/detection/ui/defaults/menu/Menu.kt
+++ kiosk/app/src/main/java/com/swuniv/agefree/presentation/detection/ui/defaults/menu/Menu.kt
@@ -4,27 +4,40 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
+/*
+* @Parameters
+* opt1 : cold, hot
+* opt2 : soft, normal, deep
+* opt3 : normal, many, very_many
+* in_out : in, out
+* gender : M, F
+* */
+
+/*
+* 디저트류 같이 옵션 없는 것들은 '-' default 값 지정
+* */
+
 class Menu(
     @SerializedName("menu")
-    var name: String,
+    var name: String = "",
     @SerializedName("price")
     @Expose(deserialize = false, serialize = false)
-    var price: Int,
+    var price: Int = 0,
     @SerializedName("image")
     @Expose(deserialize = false, serialize = false)
-    var image: Int,
+    var image: Int?,
     @SerializedName("age")
-    var age: Int,
+    var age: Int = 1,
     @SerializedName("gender")
-    var gender: String,
+    var gender: String = "M",
     @SerializedName("opt1")
-    var option1: String,
+    var option1: String = "-",
     @SerializedName("opt2")
-    var option2: String,
+    var option2: String = "-",
     @SerializedName("opt3")
-    var option3: String,
+    var option3: String = "-",
     @SerializedName("num")
-    var orderCount: Int,
+    var orderCount: Int = 0,
     @SerializedName("in_out")
-    var inOut: String
+    var inOut: String = "in"
 ) : Serializable
--- kiosk/app/src/main/java/com/swuniv/agefree/presentation/detection/ui/olds/OldSelectColdHotFragment.kt
+++ kiosk/app/src/main/java/com/swuniv/agefree/presentation/detection/ui/olds/OldSelectColdHotFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.swuniv.agefree.databinding.FragmentOldSelectColdHotBinding
 import com.swuniv.agefree.presentation.detection.ui.defaults.menu.Menu
