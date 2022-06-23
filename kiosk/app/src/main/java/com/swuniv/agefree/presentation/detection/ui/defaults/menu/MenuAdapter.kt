d6bd0110b80242ad7cce0ec37d0c54625ebd1495
gkrlsanjf <gkrlsanjf@naver.com>
2022-06-23 12:28:37 +0900

    setOnClickListener 구현

--- kiosk/app/src/main/java/com/swuniv/agefree/presentation/detection/ui/defaults/menu/MenuAdapter.kt
+++ kiosk/app/src/main/java/com/swuniv/agefree/presentation/detection/ui/defaults/menu/MenuAdapter.kt
@@ -4,39 +4,47 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.swuniv.agefree.R
+import com.swuniv.agefree.databinding.ItemDefaultMenuBinding
 import java.text.DecimalFormat
 
-class MenuAdapter(private val context: Context) : RecyclerView.Adapter<MenuAdapter.ViewHolder>() {
+class MenuAdapter(
+    private val context: Context,
+    private val menuList: ArrayList<Menu>,
+    private val onItemClickListener: ((Int) -> Unit)
+) : RecyclerView.Adapter<MenuAdapter.ViewHolder>() {
 
-    var menuList = mutableListOf<Menu>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_default_menu, parent, false)
-        return ViewHolder(view)
+        val binding = ItemDefaultMenuBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding, onItemClickListener)
     }
 
     override fun getItemCount(): Int = menuList.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(menuList[position])
+        holder.bind(menuList[position], position)
     }
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val name: TextView = itemView.findViewById(R.id.menu_name)
-        private val price: TextView = itemView.findViewById(R.id.menu_price)
-        private val image: ImageView = itemView.findViewById(R.id.menu_img)
-
-        fun bind(item: Menu) {
+    inner class ViewHolder(
+        private val binding: ItemDefaultMenuBinding,
+        private val onItemClickListener: (Int) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Menu, position: Int) {
             val dec = DecimalFormat("#,###")
-
-            name.text = item.name
-            price.text = "${dec.format(item.price)}원"
-            Glide.with(context).load(item.image).into(image)
+            binding.root.setOnClickListener {
+                onItemClickListener.invoke(position)
+            }
+            binding.menuName.text = item.name
+            binding.menuPrice.text = "${dec.format(item.price)}원"
+            Glide.with(context).load(item.image).into(binding.menuImg)
         }
     }
 
